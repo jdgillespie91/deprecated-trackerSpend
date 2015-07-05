1f3d041a1c286e4aa31fb0b235445e03fdbefa65
fatjakep <fatjakep@hotmail.com>
2015-07-05 23:21:14 +0100

    Add open_worksheet functionality

--- utils/parse_worksheet.py
+++ utils/parse_worksheet.py
@@ -1,5 +1,19 @@
-def __open_worksheet():
-    pass
+import gspread
+import json
+import os
+from oauth2client.client import SignedJwtAssertionCredentials
+
+
+def __open_worksheet(auth_key_path, workbook_key, worksheet_name):
+    with open(auth_key_path) as key:
+        json_key = json.load(key)
+
+    credentials = SignedJwtAssertionCredentials(service_account_name=json_key['client_email'], private_key=bytes(json_key['private_key'], 'UTF-8'), scope='https://spreadsheets.google.com/feeds')
+    session = gspread.authorize(credentials)
+    workbook = session.open_by_key(workbook_key)
+    worksheet = workbook.worksheet(worksheet_name)
+
+    return worksheet
 
 def __get_data():
     pass
@@ -7,5 +21,15 @@ def __get_data():
 def __write_data():
     pass
 
-def parse_worksheet():
-    pass
+def parse_worksheet(auth_key_path, workbook_key, worksheet_name):
+    worksheet = __open_worksheet(auth_key_path, workbook_key, worksheet_name)
+
+    return worksheet
+
+
+if __name__ == '__main__':
+    auth_key_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'google_key.json')
+    workbook_key = '1FqFrrbshmgdEJA1mqABuG_xp8Sc_SdZFjLCgP3DHVzk'
+    worksheet_name = 'responses'
+
+    parse_worksheet(auth_key_path, workbook_key, worksheet_name)
