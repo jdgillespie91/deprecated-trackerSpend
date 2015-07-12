daae4180f1eedc73698d6647b9b3d83a6894b3e9
fatjakep <fatjakep@hotmail.com>
2015-07-12 11:31:39 +0100

    Add modules to init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,4 @@
 from .export_worksheet import export_worksheet
 from .publish_message import publish_message
+from .get_message import get_message
+from .bind_queue_to_exchange import bind_queue_to_exchange
