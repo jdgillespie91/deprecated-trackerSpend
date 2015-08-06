0c0190c9505197bd8e9671580bd6aa776bc8b04a
fatjakep <fatjakep@hotmail.com>
2015-08-06 20:28:09 +0100

    Revert "Revert "Remove redundant functions (one too many levels of abstraction)@""
    This reverts commit 34fda0b20a87b94d7413054bfcfc81dad0ecde19.

--- utils/get_message.py
+++ utils/get_message.py
@@ -2,12 +2,6 @@ import amqp
 from contextlib import closing
 
 
-def __get_channel(connection):
-    return connection.channel()
-
-def __get_message_from_queue(channel, queue):
-    return channel.basic_get(queue=queue)
-
 def get_message(queue):
     """ Get the first message from a queue.
 
@@ -22,5 +16,5 @@ def get_message(queue):
 
     """
     with closing(amqp.Connection()) as connection:
-        channel = __get_channel(connection)
-        return  __get_message_from_queue(channel, queue)
+        channel = connection.channel()
+        return channel.basic_get(queue=queue)
