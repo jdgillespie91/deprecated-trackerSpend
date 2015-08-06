2d1ed31b5408e07967cddf5aaf0c7d380adb3b55
Jake <jdgillespie91@gmail.com>
2015-08-06 22:40:31 +0000

    Progress on publish tests (still failing though)

--- tests/utils/publish_message_integration_tests.py
+++ tests/utils/publish_message_integration_tests.py
@@ -5,39 +5,23 @@ from utils import publish_message
 
 class PublishMessageIntegrationTests(unittest.TestCase):
     def setUp(self):
-        connection = amqp.Connection()
-        self.channel = connection.channel()
-
-        self.message_body = 'test_message'
-        self.message = amqp.Message(self.message_body)
         self.exchange = 'test_exchange'
         self.type = 'direct'
+        self.message = amqp.Message('test_message')
+        self.exchange = 'test_exchange'
         self.routing_key = 'test_routing_key'
-        publish_message(self.message_body, self.exchange, self.type, self.routing_key)
+        self.connection = amqp.Connection()
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(self.exchange, self.type)
 
-    def test_test_exchange_is_declared(self):
-        self.channel.exchange_declare(exchange=self.exchange, type=self.type, passive=True)
-
-    def test_random_exchange_is_not_declared(self):
+    def test_error_if_exchange_does_not_exist(self):
         with self.assertRaises(amqp.exceptions.NotFound):
-            self.channel.exchange_declare(exchange='random_exchange', type=self.type, passive=True)
+            publish_message(message=self.message, exchange='this_exchange_does_not_exist', routing_key=self.routing_key)
 
-    def test_declared_exchange_is_durable(self):
-        with self.assertRaises(amqp.exceptions.PreconditionFailed):
-            self.channel.exchange_declare(exchange=self.exchange, type=self.type, durable=False)
+    def test_no_error_if_no_exchange_and_no_routing_key_are_passed(self):
+         publish_message(message=self.message)
 
-    def test_declared_exchange_does_not_auto_delete(self):
-        with self.assertRaises(amqp.exceptions.PreconditionFailed):
-            self.channel.exchange_declare(exchange=self.exchange, type=self.type, auto_delete=True)
-
-    def test_declared_exchange_is_correct_type(self):
-        with self.assertRaises(amqp.exceptions.PreconditionFailed):
-            self.channel.exchange_declare(exchange=self.exchange, type='fanout')
-
-    def test_message_is_published_to_correct_exchange(self):
-        self.channel.basic_publish_confirm(msg=self.message, exchange=self.exchange, routing_key=self.routing_key)
-
-    def test_published_message_has_correct_routing_key(self):
+    def skip_test_published_message_has_correct_routing_key(self):
         queue = 'test_queue'
 
         try:
--- utils/publish_message.py
+++ utils/publish_message.py
@@ -2,14 +2,14 @@ import amqp
 from contextlib import closing
 
 
-def publish_message(message_body, exchange, type, routing_key):
+def publish_message(message, exchange='', routing_key=''):
     """ Publish a message to an exchange with exchange type and routing key specified.
 
     A message is sent to a specified exchange with the provided routing_key.
 
-    :param message_body: The body of the message to be sent.
-    :param exchange: The name of the exchange the message is sent to.
-    :param routing_key: The routing key to be sent with the message.
+    :param message: The body of the message to be sent.
+    :param exchange (optional): The name of the exchange the message is sent to. If blank, the default exchange is used.
+    :param routing_key (optional): The routing key to be sent with the message.
 
     Usage::
 
