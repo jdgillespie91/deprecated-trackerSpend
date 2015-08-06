464a3f79ea2e6ea927fd30b72ce6aec8811c8570
Jake <jdgillespie91@gmail.com>
2015-08-06 22:25:56 +0000

    Update integration tests for new functionality

--- tests/utils/get_message_integration_tests.py
+++ tests/utils/get_message_integration_tests.py
@@ -5,50 +5,34 @@ from utils import get_message
 
 class GetMessageIntegrationTests(unittest.TestCase):
     def setUp(self):
-        connection = amqp.Connection()
-        self.channel = connection.channel()
-
         self.queue = 'test_queue'
-        get_message(self.queue)
+        self.connection = amqp.Connection()
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(self.queue)
 
-    def test_test_queue_is_declared(self):
-        self.channel.queue_declare(queue=self.queue, passive=True)
-
-    def test_random_queue_is_not_declared(self):
+    def test_error_if_queue_does_not_exist(self):
         with self.assertRaises(amqp.exceptions.NotFound):
-            self.channel.queue_declare(queue='random_queue', passive=True)
+            get_message(queue='this_queue_does_not_exist')
 
-    def test_declared_queue_is_durable(self):
-        with self.assertRaises(amqp.exceptions.PreconditionFailed):
-            self.channel.queue_declare(queue=self.queue, durable=False, auto_delete=False)
+    def test_error_if_no_queue_is_passed(self):
+        with self.assertRaises(amqp.exceptions.NotFound):
+            get_message()
 
-    def test_declared_queue_does_not_auto_delete(self):
-        with self.assertRaises(amqp.exceptions.PreconditionFailed):
-            self.channel.queue_declare(queue=self.queue, durable=True, auto_delete=True)
+    def test_returns_none_if_queue_exists_and_no_message_in_queue(self):
+        self.assertIs(get_message(queue=self.queue), None)
 
-    def test_declared_queue_is_not_exclusive(self):
-        with self.assertRaises(amqp.exceptions.PreconditionFailed):
-            self.channel.queue_declare(queue=self.queue, durable=True, auto_delete=False, exclusive=True)
+    def test_returns_message_if_queue_exists_and_message_in_queue(self):
+        self.channel.basic_publish(msg=amqp.Message('message'), routing_key=self.queue)
+        self.assertIsInstance(get_message(queue=self.queue), amqp.basic_message.Message)
 
-    def test_message_is_retrieved_if_message_exists_in_queue(self):
-        exchange = 'test_exchange'
-        type = 'fanout'
-        queue = 'local_test_queue'
-        message = amqp.Message('test_message')
-
-        try:
-            self.channel.exchange_declare(exchange=exchange, type=type, durable=True, auto_delete=False)
-            self.channel.queue_declare(queue=queue, durable=True, auto_delete=False)
-            self.channel.queue_bind(queue=queue, exchange=exchange)
-            self.channel.basic_publish_confirm(msg=message, exchange=exchange, routing_key='')
-            message = get_message(queue)
-        finally:
-            self.channel.queue_delete(queue=queue)
-            self.channel.exchange_delete(exchange=exchange)
-
-        self.assertIsNotNone(message)
+    def test_returned_message_has_correct_attributes(self):
+        self.channel.basic_publish(msg=amqp.Message('message'), routing_key=self.queue)
+        message = get_message(queue=self.queue)
         self.assertTrue(hasattr(message, 'body'))
-        self.assertEqual(message.body, 'test_message')
+        self.assertEqual(message.body, 'message')
+
+    def skip_test_message_is_removed_from_queue_after_getting(self):
+        pass
 
     def tearDown(self):
         self.channel.queue_delete(queue=self.queue)
