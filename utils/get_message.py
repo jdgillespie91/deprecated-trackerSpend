586779c666dfeee9c067ca85efb2319228c507d2
fatjakep <fatjakep@hotmail.com>
2015-08-09 00:28:21 +0100

    Finalise docstring

--- utils/get_message.py
+++ utils/get_message.py
@@ -5,7 +5,7 @@ from contextlib import closing
 def get_message(queue):
     """ Get the first message from a queue.
 
-    The first message from a queue is retrieved. If there is no such message, the function exits quietly. 
+    The first message from a queue is retrieved. If the queue does not exist, an amqp.exception.NotFound error is raised. If the queue exists and there is no message, a None object is returned. If the queue exists and there is a message, it is returned as an amqp.Message object.
 
     :param queue: The name of the queue from which to get the message.
 
